f77cdbdc791e7ee4dd14c7793a0bfddd46084070
soarflat <soarflat777@gmail.com>
2021-05-23 18:41:12 +0900

    空文字のタスクを追加できないようにするバリデーションを追加

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,6 +1,11 @@
+import { IsNotEmpty } from 'class-validator';
+
 // DTO（Data Transfer Object）スキーマ
 // DTO はネットワーク上でどのようにデータを送信するかを定義するオブジェクト
 export class CreateTaskDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
 }
