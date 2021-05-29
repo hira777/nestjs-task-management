14b3142c791d7d0cbcbb5c2fbcbbb0955bedc1ee
soarflat <soarflat777@gmail.com>
2021-05-29 14:52:39 +0900

    DB に接続するための記述をする箇所を変更

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 15432,
+      username: 'admin',
+      password: 'admin',
+      database: 'taskmanagement',
+      // エンティティを自動で読み込む
+      autoLoadEntities: true,
+      // アプリケーション実行時にエンティティを DB スキーマに反映する
+      synchronize: true,
+    }),
+    TasksModule,
+  ],
 })
 export class AppModule {}
